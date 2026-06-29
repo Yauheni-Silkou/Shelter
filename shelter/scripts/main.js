3c42be2a718e8847945f77f3ef51c2d10c156b61
Yauheni-Silkou <yauheni.silkou@outlook.com>
2026-06-29 11:22:20 +0300

    feat: complete infinite carousel slider module (Mon, Jun 29, 2026 11:22:20 AM)

--- shelter/scripts/main.js
+++ shelter/scripts/main.js
@@ -2,4 +2,10 @@ import { initBurger } from "./burger.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
   initBurger();
+
+  const carouselTrack = document.getElementById("slider-track");
+  if (carouselTrack) {
+    const { initCarousel } = await import("./carousel.js");
+    initCarousel(carouselTrack);
+  }
 });
